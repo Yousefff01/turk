faf57190681776421869a0e0b905834b38cc6467
Yusuf Talha GÜR <yousef0.01@hotmail.com>
2025-05-24 00:33:58 +0300

    first load

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const turkButton = document.getElementById("turk");
+const kurtButton = document.getElementById("kurt");
+let acceptSize = 12;
+let rejectSize = 12;
+let rejectPadding = [15, 50];
+
+turkButton.addEventListener("click", () => {
+  const bodyChildren = document.body.children;
+
+  for (let i = 0; i < bodyChildren.length; i++) {
+    const element = bodyChildren[i];
+    if (element.id.toLowerCase() !== "turkimg") {
+      element.style.display = "none";
+    }
+    if (element.id.toLowerCase() === "turkimg") {
+      element.style.display = "flex";
+    }
+  }
+});
+kurtButton.addEventListener("click", () => {
+  const bodyChildren = document.body.children;
+
+  for (let i = 0; i < bodyChildren.length; i++) {
+    const element = bodyChildren[i];
+    if (element.id.toLowerCase() !== "kurtimg") {
+      element.style.display = "none";
+    }
+
+    if (element.id.toLowerCase() === "kurtimg") {
+      element.style.display = "flex";
+    }
+  }
+});
